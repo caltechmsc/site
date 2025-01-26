10d8b3a730f1aff4185024dc80eba5cbdfd7867e
Tony Kan <tianchengkan@gmail.com>
2025-01-25 21:13:56 -0800

    feat(backend/db): Add collaborators database connection and management
    - Added connection to `collaborators.db` in `db.js`.
    - Implemented functionality to create `collaborators` table if it does not exist.
    - Updated `closeDbs` function to include closing the `collaborators` database connection.
    - Ensured proper error handling and logging for the `collaborators` database operations.

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -14,7 +14,7 @@ if (!fs.existsSync(dbPath)) {
   fs.mkdirSync(dbPath);
 }
 
-// Connect to the admins, and members databases
+// Connect to the admins, members, and collaborators databases
 const adminsDb = new sqlite3.Database(dbPath + '/admins.db', (err) => {
   if (err) {
     console.error(err.message);
@@ -29,8 +29,18 @@ const membersDb = new sqlite3.Database(dbPath + '/members.db', (err) => {
     console.log('Connected to the members database.');
   }
 });
+const collaboratorsDb = new sqlite3.Database(
+  dbPath + '/collaborators.db',
+  (err) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log('Connected to the collaborators database.');
+    }
+  },
+);
 
-// Close the admins, and members databases
+// Close the admins, members, and collaborators databases
 const closeDbs = () => {
   adminsDb.close((err) => {
     if (err) {
@@ -47,9 +57,17 @@ const closeDbs = () => {
       console.log('Closed the members database connection.');
     }
   });
+
+  collaboratorsDb.close((err) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log('Closed the collaborators database connection.');
+    }
+  });
 };
 
-// Create the admins, and members tables if they do not exist
+// Create the admins, members, and collaborators tables if they do not exist
 adminsDb.serialize(() => {
   adminsDb.run(
     'CREATE TABLE IF NOT EXISTS admins (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT NOT NULL UNIQUE, password TEXT NOT NULL)',
@@ -58,6 +76,11 @@ adminsDb.serialize(() => {
 membersDb.serialize(() => {
   membersDb.run(
     'CREATE TABLE IF NOT EXISTS members (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, identity TEXT NOT NULL, email TEXT NOT NULL, expertise TEXT NOT NULL, profile TEXT)',
+  );
+});
+collaboratorsDb.serialize(() => {
+  collaboratorsDb.run(
+    'CREATE TABLE IF NOT EXISTS collaborators (id INTEGER PRIMARY KEY AUTOINCREMENT, organization TEXT NOT NULL, country TEXT NOT NULL, leader TEXT NOT NULL, email TEXT NOT NULL, website TEXT, latitude REAL, longitude REAL)',
   );
 });
 
